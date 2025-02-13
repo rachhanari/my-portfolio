a0c5764d72e674244d52686f0a4419b2118effef
rachhanari <racchanarendra123@gmail.com>
2025-02-14 02:25:37 +0530

    changes

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,7 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  css: {
-    modules: {
-      localsConvention: "camelCase",
-    },
-  },
+  base: "/my-portfolio/", // Change to your repo name
 });
