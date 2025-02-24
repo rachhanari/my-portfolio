1f4924e73d68687c949bab2f26bd80e67266f098
rachhanari <racchanarendra123@gmail.com>
2025-02-25 00:02:13 +0530

    changed

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import styles from "./About.module.css";
 import { getImageUrl } from "../../utils";
-import img1 from "../../../dist/assets/about/MyNewImg.png"
+import img1 from "../../../dist/assets/about/MyImg.png"
 
 export const About = () => {
   return (
--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import styles from "./Hero.module.css";
-import img1 from "../../../dist/assets/hero/MyImg.png";
+import img1 from "../../../dist/assets/hero/MyNew.png";
 
 export const Hero = () => {
   return (
